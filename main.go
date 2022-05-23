130d3ef59b65cf212bc40c283cdefa054c4ce9bb
Kaiya <kaiya.xiong337@imaygou.com>
2022-05-23 20:51:07 +0800

    update ci

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"strings"
 
 	"github.com/Kaiya/todo/dao"
 	"github.com/gin-gonic/gin"
@@ -14,6 +17,14 @@ var (
 
 func main() {
 	flag.Parse()
+	err := SetFlagsFromEnvironment()
+	if err != nil {
+		panic(err)
+	}
+	if *sqlPass == "" {
+		*sqlPass = os.Getenv("sqlPass")
+	}
+
 	if *sqlUser == "" || *sqlPass == "" {
 		panic("sqlPass is empty")
 	}
@@ -28,4 +39,18 @@ func main() {
 		api.DELETE("/:id", dao.DeleteTodo)
 	}
 	router.Run()
+}
+
+func SetFlagsFromEnvironment() (err error) {
+	flag.VisitAll(func(f *flag.Flag) {
+		name := strings.ToUpper(strings.Replace(f.Name, "-", "_", -1))
+		if value, ok := os.LookupEnv(name); ok {
+			err2 := flag.Set(f.Name, value)
+			if err2 != nil {
+				err = fmt.Errorf("failed setting flag from environment: %w", err2)
+			}
+		}
+	})
+
+	return
 }
